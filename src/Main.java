a66e8d82ca400f5022c9837363c0b11054457022
ArtemLugovkin <artem-lugovkin@mail.ru>
2022-09-21 21:44:59 +0500

    выполнил дз номер 1

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!!");
+        System.out.println("Пишем на дз");
+        System.out.println("Делаем комит и пуш");
+        System.out.println("Переходим в гитхаб");
+
+
     }
 }
